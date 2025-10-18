43fe27db8ac099f55b753752fe0795b6d3cf758f
famiaattaria <famiaattaria1112@gmail.com>
2025-10-18 09:21:59 -0700

    updated env

--- userSeed.js
+++ userSeed.js
@@ -1,4 +1,4 @@
-import { User } from "./models/user.js";
+import  User from "./models/user.js";
 import bcrypt from "bcrypt";
 import { connectDB } from "./lib/db.js";
 const userRegister = async () => {
