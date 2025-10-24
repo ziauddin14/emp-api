fe63a204791fd5f78827cc822af3769545ce8a50
Zia Uddin <ziamaryam2526@gmail.com>
2025-10-24 05:18:07 -0700

    Change upload field name from 'image' to 'profileImage'

--- routes/employee.js
+++ routes/employee.js
@@ -11,7 +11,7 @@ import upload from "../middlewares/upload.js";
 const router = express.Router();
 
 router.get("/", getEmployee);
-router.post("/add", upload.single("image"), addEmployee);
+router.post("/add", upload.single("profileImage"), addEmployee);
 router.get("/single/:id", getEmployees);
 router.put("/update/:id", updateEmployee);
 router.get('/department/:id', fetchEmployeesByDepId)
